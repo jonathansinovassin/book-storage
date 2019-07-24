7be14fda732b37c4a15749c9d8929e1a87f6ee93
jonathansinovassin <j.sinovassin@gmail.com>
2019-07-24 23:52:23 +0200

    initialize project

---
+++ src/main/resources/script.sql
@@ -0,0 +1,11 @@
+create role book_manager with LOGIN encrypted password 'book_manager_pass';
+
+create database books owner = book_manager;
+
+create table book(
+	id serial primary key,
+	author character varying(255),
+	price double precision,
+	summary text,
+	date_of_publication date
+);
