c21e9a9c7bc5df143e028f7cc49516da2357c058
jonathansinovassin <j.sinovassin@gmail.com>
2019-08-01 01:41:41 +0200

    -fix config hibernate

--- src/main/java/data/Util/HibernateUtil.java
+++ src/main/java/data/Util/HibernateUtil.java
@@ -1,42 +1,42 @@
-package data.repository.Util;
-
-import java.util.HashMap;
-import java.util.Map;
+package data.Util;
 
 import org.hibernate.SessionFactory;
 import org.hibernate.boot.Metadata;
 import org.hibernate.boot.MetadataSources;
 import org.hibernate.boot.registry.StandardServiceRegistry;
 import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
-/**
- * @author imssbora
- */
+import data.model.Book;
+
 public class HibernateUtil {
-  private static StandardServiceRegistry registry;
-  private static SessionFactory sessionFactory;
+    private static StandardServiceRegistry registry;
+    private static SessionFactory sessionFactory;
 
-  public static SessionFactory getSessionFactory() {
-    if (sessionFactory == null) {
-      try {
-        StandardServiceRegistry standardRegistry =
-                new StandardServiceRegistryBuilder().configure("hibernate.cfg.xml").build();
-        Metadata metaData =
-                new MetadataSources(standardRegistry).getMetadataBuilder().build();
-        sessionFactory = metaData.getSessionFactoryBuilder().build();
-      } catch (Exception e) {
-        System.out.println("SessionFactory creation failed");
-        if (registry != null) {
-          StandardServiceRegistryBuilder.destroy(registry);
+    private static Logger LOGGER = LoggerFactory.getLogger(HibernateUtil.class);
+
+    public static SessionFactory getSessionFactory() {
+        if (sessionFactory == null) {
+            try {
+                StandardServiceRegistry standardRegistry =
+                        new StandardServiceRegistryBuilder().configure("hibernate.cfg.xml").build();
+                Metadata metaData =
+                        new MetadataSources(standardRegistry).addAnnotatedClass(Book.class).getMetadataBuilder().build();
+                sessionFactory = metaData.getSessionFactoryBuilder().build();
+            } catch (Exception e) {
+                LOGGER.error("SessionFactory creation failed", e);
+                if (registry != null) {
+                    StandardServiceRegistryBuilder.destroy(registry);
+                }
+            }
         }
-      }
+        return sessionFactory;
     }
-    return sessionFactory;
-  }
 
-  public static void shutdown() {
-    if (registry != null) {
-      StandardServiceRegistryBuilder.destroy(registry);
+    public static void shutdown() {
+        if (registry != null) {
+            StandardServiceRegistryBuilder.destroy(registry);
+        }
     }
-  }
 }
